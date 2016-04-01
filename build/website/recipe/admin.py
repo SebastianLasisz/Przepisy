4bbf71de3330bea6b7d5e50ed845431b2adfe2b3
Sebastian Łasisz <slasisz@gmail.com>
2016-04-01 17:07:43 +0200

    Update of admin panel

--- build/website/recipe/admin.py
+++ build/website/recipe/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 from recipe.models import *
 
-admin.site.register(Recipe)
-admin.site.register(Ingredient)
-admin.site.register(Meal)
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'global_access')
+admin.site.register(Recipe, RecipeAdmin)
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'value', 'unit')
+admin.site.register(Ingredient, IngredientAdmin)
+
+
+class MealAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'date')
+admin.site.register(Meal, MealAdmin)
+
+
 admin.site.register(ShoppingList)
 admin.site.register(ProductList)
