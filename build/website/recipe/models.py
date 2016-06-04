34232a5163d86ab5d8108224f810de759c8e966e
Sebastian Łasisz <slasisz@gmail.com>
2016-06-04 16:49:14 +0200

    Templated refactoring

--- build/website/recipe/models.py
+++ build/website/recipe/models.py
@@ -91,6 +91,10 @@ class RecipeComment(models.Model):
     recipe = models.ForeignKey(Recipe)
     user = models.ForeignKey(User)
     comment = models.CharField(max_length=1000, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
 
 
 class Meal(models.Model):
